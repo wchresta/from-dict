4e01ee9645e57b69b7711481c22c3a426c70ee31
jcal-15 <13785195+jcal-15@users.noreply.github.com>
2022-11-13 15:38:05 -0800

    Updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="from-dict",
-    version="0.3.1",
+    version="0.3.2",
     packages=find_packages(),
     author="Wanja Chresta",
     author_email="wanja.hs@chrummibei.ch",
