2d7530b9ab867099fbb90ed98419e4d00966f270
jcal-15 <13785195+jcal-15@users.noreply.github.com>
2023-06-15 22:17:31 -0700

    Removing support Python 3.7 - Literals are not supported in this version - End of support is 2023-06-27

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -23,7 +22,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=[],
-    tests_require=["pytest", "attr"],
+    tests_require=["pytest", "attrs"],
 )
